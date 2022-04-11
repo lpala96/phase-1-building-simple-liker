d4fe80171e49cacbb7a5887215d0de5517a15a73
Loyda <palacioloydita@gmail.com>
2022-04-11 13:33:04 -0700

    commit tree 639d1c27634bbf25881c035e08cbb6764641202d

--- main.js
+++ main.js
@@ -4,8 +4,66 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+  const hearts = document.querySelectorAll('.like-glyph')
+
+  function clickHeart (e) {
+    const heart = e.target
+    mimicServerCall('bogusURL')
+    .then( () => {
+      if (heart.innerText === EMPTY_HEART ) {
+          heart.innertText = FULL_HEART
+          heart.className = 'activated-heart'
+          
+      }
+      else {
+        heart.innerText = EMPTY_HEART
+        heart.className = ''
+
+    }})
 
 
+      .catch((error) => {
+        const modal = document.getElementById('modal')
+        console.log(modal)
+            modal.className = ""
+            modal.innerText = error
+            setTimeout(() => modal.className = "hidden", 3000)
+              
+          })
+
+  }
+
+  for (const element of hearts) {
+    element.addEventListener('click', clickHeart)
+  }
+  console.log(hearts)
+//   hearts.forEach(hearts => hearts.addEventListener('click', clickLike))
+
+//   function clickLike(hearts) {
+//     console.log(hearts.target)
+//     mimicServerCall()
+//     .then( () => {
+//       if (hearts.target.innerText === EMPTY_HEARTS) {
+//         hearts.target.classList.add('activated-heart')
+//         hearts.target.innertText = FULL_HEART
+//       }
+//       else {
+//         hearts.target.classList.remove('activated-heart')
+//           hearts.target.innerText = EMPTY_HEART
+//       }
+
+//     })
+//   .catch(() => {
+//     const modal = document.querySelectorByID('modal')
+//     console.log(modal)
+//     modal.className = "show"
+
+//     setTimeout(() => {
+//       modal.className = "hidden"}, 3000)
+      
+//   })
+
+// }
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
